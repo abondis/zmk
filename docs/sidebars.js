1a7012e98dd498959346d222d22a0454fa93acc8
Joel Spadin <joelspadin@gmail.com>
2022-05-21 17:48:27 -0500

    feat(docs): Update power and lighting config pages
    Moved battery configuration to its own page to match the feature page.
    
    Documented that external power is disabled when in sleep mode.
    
    Clarified that the *_START configs apply on first boot, and any changes
    after that are persisted.

--- docs/sidebars.js
+++ docs/sidebars.js
@@ -56,6 +56,7 @@ module.exports = {
     Configuration: [
       "config/index",
       "config/backlight",
+      "config/battery",
       "config/behaviors",
       "config/combos",
       "config/displays",
@@ -63,8 +64,8 @@ module.exports = {
       "config/keymap",
       "config/kscan",
       "config/power",
-      "config/system",
       "config/underglow",
+      "config/system",
     ],
     Development: [
       "development/clean-room",
